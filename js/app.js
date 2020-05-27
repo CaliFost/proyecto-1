e40512b08c0d29c3be9b69115b466a5189424ab3
CaliFost <35895111+CaliFost@users.noreply.github.com>
2020-05-27 20:31:10 -0300

    Suma de cambio de tema
    Se incorpora la funcionalidad para cambiar entre tema oscuro y claro.
    Se incorpora el uso de Bootstrap.
    Aún queda acomodar las grillas correctamente

--- js/app.js
+++ js/app.js
@@ -114,3 +114,25 @@ class target {
         };
     }
 }
+
+
+// Using JavaScript to change the theme programmatically (CSS)
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
